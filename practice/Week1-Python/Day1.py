175b482e2dcecea182eea18342706b903d91cdcf
Saloni Nigam <saloninigam2311@gmail.com>
2026-06-22 16:43:10 +0530

    Day 1: Variable practice done

---
+++ practice/Week1-Python/Day1.py
@@ -0,0 +1,20 @@
+#  variables
+
+name=" Alex"
+age= 20
+city="United State"
+learning= "Python"
+
+
+## print using f string
+
+print(f"Name: {name}")
+print(f"age: {age}")
+print(f"city: {city}")
+print(f"learning: {learning}")
+
+
+## data type
+
+print(" Data type of Name:", type(name))
+print("Data type of age:", type(age))
