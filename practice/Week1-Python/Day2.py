a1430f1617dbfed700a68b73cf8b7a5de16ab760
Saloni Nigam <saloninigam2311@gmail.com>
2026-06-24 10:45:25 +0530

    Day 2

---
+++ practice/Week1-Python/Day2.py
@@ -0,0 +1,37 @@
+# input from user
+
+'''name=input("Enter your name ")
+print (name)'''
+
+#indentation is use as a block by defaut it has 4 time space  
+
+
+# if elif and else
+
+'''marks=int(input("Enter your marks "))
+if marks>=90:
+    print("Grade A")
+elif marks>=80:
+    print("Grade B")
+else:
+    print("focus on your study")'''
+
+
+#Type Casting 
+
+"""x='10'
+y=10
+z=28.3
+print(type(x))
+print(type(y))
+print(type(z))
+
+y=int(x)
+z=float(x)
+print(type(y))
+print(type(z))
+
+#note by default input is always in string 
+age=input("enter age ")
+print(type(age))"""
+
