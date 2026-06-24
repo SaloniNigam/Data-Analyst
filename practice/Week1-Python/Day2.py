582d910ed5b298d52e1ca17a583918798e03b247
Saloni Nigam <saloninigam2311@gmail.com>
2026-06-24 10:49:11 +0530

    Day 2: Basic

--- practice/Week1-Python/Day2.py
+++ practice/Week1-Python/Day2.py
@@ -1,25 +1,25 @@
 # input from user
 
-'''name=input("Enter your name ")
-print (name)'''
+name=input("Enter your name ")
+print (name)
 
 #indentation is use as a block by defaut it has 4 time space  
 
 
 # if elif and else
 
-'''marks=int(input("Enter your marks "))
+marks=int(input("Enter your marks "))
 if marks>=90:
     print("Grade A")
 elif marks>=80:
     print("Grade B")
 else:
-    print("focus on your study")'''
+    print("focus on your study")
 
 
 #Type Casting 
 
-"""x='10'
+x='10'
 y=10
 z=28.3
 print(type(x))
@@ -33,5 +33,5 @@ print(type(z))
 
 #note by default input is always in string 
 age=input("enter age ")
-print(type(age))"""
+print(type(age))
 
